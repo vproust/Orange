772d9baee7a76bc3504d3ee43ffa51c0b0d061c7
vproust <valentin.proust@etu.univ-nantes.fr>
2013-11-24 19:55:58 +0100

    modification de Image

--- src/model/Image.java
+++ src/model/Image.java
@@ -5,15 +5,66 @@ import java.util.Set;
 public class Image {
 	private String imageName;
 	private Set<Mosaic> mosaics;
-	private int numberofRows;
-	private int numberofColumns;
+	private int numberOfRows;
+	private int numberOfColumns;
 	private int mosaicHeight;
 	private int mosaicWidth;
-	public Image(int numberofRows, int numberofColumns) {
-		super();
-		NumberofRows = numberofRows;
-		NumberofColumns = numberofColumns;
-	}
 	
+	public Image(int numberOfRows, int numberOfColumns, int mosaicHeight,
+			int mosaicWidth) {
+		super();
+		this.numberOfRows = numberOfRows;
+		this.numberOfColumns = numberOfColumns;
+		this.mosaicHeight = mosaicHeight;
+		this.mosaicWidth = mosaicWidth;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	public Set<Mosaic> getMosaics() {
+		return mosaics;
+	}
+
+	public void setMosaics(Set<Mosaic> mosaics) {
+		this.mosaics = mosaics;
+	}
+
+	public int getNumberOfRows() {
+		return numberOfRows;
+	}
+
+	public void setNumberOfRows(int numberOfRows) {
+		this.numberOfRows = numberOfRows;
+	}
+
+	public int getNumberOfColumns() {
+		return numberOfColumns;
+	}
+
+	public void setNumberOfColumns(int numberOfColumns) {
+		this.numberOfColumns = numberOfColumns;
+	}
+
+	public int getMosaicHeight() {
+		return mosaicHeight;
+	}
+
+	public void setMosaicHeight(int mosaicHeight) {
+		this.mosaicHeight = mosaicHeight;
+	}
+
+	public int getMosaicWidth() {
+		return mosaicWidth;
+	}
+
+	public void setMosaicWidth(int mosaicWidth) {
+		this.mosaicWidth = mosaicWidth;
+	}
 	
 }
