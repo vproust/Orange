87310486b78712699447f0bb82f774d9d6182c7e
vproust <valentin.proust@etu.univ-nantes.fr>
2013-11-24 20:23:53 +0100

    ajout de la communication avec l'utilisateur

--- src/controller/main.java
+++ src/controller/main.java
@@ -3,6 +3,8 @@
  */
 package controller;
 
+import java.util.Scanner;
+
 /**
  * @author fabienrichard
  *
@@ -14,7 +16,23 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ImageController imageController = new ImageController();
+		int numberOfRows;
+		int numberOfColumns;
+		int mosaicWidth;
+		int mosaicHeight;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir le nombres de lignes :");
+		numberOfRows = sc.nextInt();
+		System.out.println("Veuillez saisir le nombres de colonnes :");
+		numberOfColumns = sc.nextInt();
+		System.out.println("Veuillez saisir la largeur d'une mosaique en pixels :");
+		mosaicWidth = sc.nextInt();
+		System.out.println("Veuillez saisir la hauteur d'une mosaique en pixels :");
+		mosaicHeight = sc.nextInt();
+		
+		//imageController.createImage();
 	}
 
 }
