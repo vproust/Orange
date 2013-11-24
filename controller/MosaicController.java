e7224688afb2211edd3dfcc0ae39b7d19667ecb2
richfab <fabien.richard@etu.univ-nantes.fr>
2013-11-24 19:43:22 +0100

    Ajout du controller

---
+++ controller/MosaicController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package controller;
+
+import model.Film;
+
+/**
+ * @author fabienrichard
+ *
+ */
+public class MosaicController {
+	public Mosaic createMosaic(){
+		return new Mosaic();
+	}
+	public void addFilmToMosaic(Film film, MosaicPosition mosaicPosition){
+		
+	}
+}
