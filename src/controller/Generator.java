351dcca8ad1a6b16c6e48e812248d95a8a4dc580
vproust <valentin.proust@etu.univ-nantes.fr>
2014-01-09 18:46:45 +0100

    nettoyage

--- src/controller/Generator.java
+++ src/controller/Generator.java
@@ -1,35 +1,37 @@
 package controller;
 
-<<<<<<< HEAD
 import model.*;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class Generator {
-	
+
 	public boolean generateLevels(String logFilePath){
-		
+
 		Mosaic mosaic = new Mosaic(new MosaicPosition(0,0,0));
 		recursiveGeneratorLevels(mosaic);
 		return true;
 	}
 
 	public BufferedImage recursiveGeneratorLevels(Mosaic mosaic){
-		
+
 		if(mosaic.getNumberOfFilms() > 100){
 			Mosaic mosaic0.0 = les films de la sous mosaique de “mosaic” en haut à gauche;
-	Mosaic mosaic0.1 = les films de la sous mosaique de “mosaic” en haut à droite;
-	Mosaic mosaic1.0 = les films de la sous mosaique de “mosaic” en bas à gauche;
-	Mosaic mosaic1.1 = les films de la sous mosaique de “mosaic” en bas à droite;
+			Mosaic mosaic0.1 = les films de la sous mosaique de “mosaic” en haut à droite;
+			Mosaic mosaic1.0 = les films de la sous mosaique de “mosaic” en bas à gauche;
+			Mosaic mosaic1.1 = les films de la sous mosaique de “mosaic” en bas à droite;
 
-	BufferedImage biMosaic0.0 =  recursiveGeneratorLevels(mosaic0.0);
-	BufferedImage biMosaic0.1 =  recursiveGeneratorLevels(mosaic0.1);
-	BufferedImage biMosaic1.0 =  recursiveGeneratorLevels(mosaic1.0);
-	BufferedImage biMosaic0.1 =  recursiveGeneratorLevels(mosaic1.1);
-			
-	BufferedImage biMosaic = clip(biMosaic0.0,biMosaic0.1,biMosaic1.0,biMosaic1.1);
-	write(biMosaic);
-	return biMosaic;
+			BufferedImage biMosaic0.0 =  recursiveGeneratorLevels(mosaic0.0);
+			BufferedImage biMosaic0.1 =  recursiveGeneratorLevels(mosaic0.1);
+			BufferedImage biMosaic1.0 =  recursiveGeneratorLevels(mosaic1.0);
+			BufferedImage biMosaic0.1 =  recursiveGeneratorLevels(mosaic1.1);
+
+			BufferedImage biMosaic = clip(biMosaic0.0,biMosaic0.1,biMosaic1.0,biMosaic1.1);
+			write(biMosaic);
+			return biMosaic;
 		}
 
 		else{
@@ -38,29 +40,12 @@ public class Generator {
 			return biMosaic;
 		}
 	}
-
-	public Mosaic logFileToMosaic(String logFilePath){	};
-
 	// ressemble à film to mosaicPosition
 	Mosaic MosaicToSubMosac(Mosaic mosaic, position){};
 
 	clip(BufferedImage biMosaic0.0, BufferedImage biMosaic0.1, BufferedImage biMosaic1.0, BufferedImage biMosaic1.1){};
 	generateMosaic(Mosaic mosaic){};
 
-=======
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
-import model.Film;
-import model.Mosaic;
-import model.MosaicPosition;
-
-public class Generator {
-	
 	/**
 	 * Cette mŽthode lis fichier texte contenant les noms de films et leur position et les ajoute dans une unique mosaique.
 	 * 
@@ -69,9 +54,9 @@ public class Generator {
 	 * @return filmSet Le conteneur des objets "film"
 	 **/
 	public boolean logFileToMosaic(String logFilePath){
-		
-		Mosaic mosaic = new Mosaic(new MosaicPosition(0,0));
-		
+
+		Mosaic mosaic = new Mosaic(new MosaicPosition(0,0,0));
+
 		BufferedReader br = null;
 
 		try {
@@ -107,9 +92,8 @@ public class Generator {
 				ex.printStackTrace();
 			}
 		}
-		
+
 		return true;
 	}
->>>>>>> e0316c83d03d93581aacd36cb40b243a95a1668b
 
 }
