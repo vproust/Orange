939d6797dbd81ab273ed9d028e4f2b3ff60f695f
vproust <valentin.proust@etu.univ-nantes.fr>
2014-01-09 18:38:39 +0100

    modifications de Generator

--- src/controller/Generator.java
+++ src/controller/Generator.java
@@ -1,5 +1,50 @@
 package controller;
 
+import model.*;
+import java.awt.image.BufferedImage;
+
+
 public class Generator {
+	
+	public boolean generateLevels(String logFilePath){
+		
+		Mosaic mosaic = new Mosaic(new MosaicPosition(0,0,0));
+		recursiveGeneratorLevels(mosaic);
+		return true;
+	}
+
+	public BufferedImage recursiveGeneratorLevels(Mosaic mosaic){
+		
+		if(mosaic.getNumberOfFilms() > 100){
+			Mosaic mosaic0.0 = les films de la sous mosaique de “mosaic” en haut à gauche;
+	Mosaic mosaic0.1 = les films de la sous mosaique de “mosaic” en haut à droite;
+	Mosaic mosaic1.0 = les films de la sous mosaique de “mosaic” en bas à gauche;
+	Mosaic mosaic1.1 = les films de la sous mosaique de “mosaic” en bas à droite;
+
+	BufferedImage biMosaic0.0 =  recursiveGeneratorLevels(mosaic0.0);
+	BufferedImage biMosaic0.1 =  recursiveGeneratorLevels(mosaic0.1);
+	BufferedImage biMosaic1.0 =  recursiveGeneratorLevels(mosaic1.0);
+	BufferedImage biMosaic0.1 =  recursiveGeneratorLevels(mosaic1.1);
+			
+	BufferedImage biMosaic = clip(biMosaic0.0,biMosaic0.1,biMosaic1.0,biMosaic1.1);
+	write(biMosaic);
+	return biMosaic;
+		}
+
+		else{
+			BufferedImage biMosaic = generateMosaic(mosaic);
+			write(mosaic);
+			return biMosaic;
+		}
+	}
+
+	public Mosaic logFileToMosaic(String logFilePath){	};
+
+	// ressemble à film to mosaicPosition
+	Mosaic MosaicToSubMosac(Mosaic mosaic, position){};
+
+	clip(BufferedImage biMosaic0.0, BufferedImage biMosaic0.1, BufferedImage biMosaic1.0, BufferedImage biMosaic1.1){};
+	generateMosaic(Mosaic mosaic){};
+
 
 }
--- src/model/MosaicPosition.java
+++ src/model/MosaicPosition.java
@@ -3,11 +3,13 @@ package model;
 public class MosaicPosition {
 	int rowNumber;
 	int columnNumber;
+	int zoomLevel;
 	
-	public MosaicPosition(int rowNumber, int columnNumber) {
+	public MosaicPosition(int rowNumber, int columnNumber, int zoomLevel){
 		super();
 		this.rowNumber = rowNumber;
 		this.columnNumber = columnNumber;
+		this.zoomLevel = zoomLevel;
 	}
 	public int getRowNumber() {
 		return rowNumber;
@@ -20,5 +22,11 @@ public class MosaicPosition {
 	}
 	public void setColumnNumber(int columnNumber) {
 		this.columnNumber = columnNumber;
+	}
+	public int getZoomLevel() {
+		return zoomLevel;
+	}
+	public void setZoomLevel(int zoomLevel) {
+		this.zoomLevel = zoomLevel;
 	}
 }
