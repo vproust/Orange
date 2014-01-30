94e7507e8b500f1803e5160176c7fa87c8593418
vproust <valentin.proust@etu.univ-nantes.fr>
2014-01-30 10:35:30 +0100

    m

--- src/laboratorie/Clip.java
+++ src/laboratorie/Clip.java
@@ -2,8 +2,11 @@ package laboratorie;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Line2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -11,37 +14,50 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Clip {
+	public static int ARR_SIZE = 20;
+	
+	 static void drawArrow(Graphics g1, int x1, int y1, int x2, int y2) {
+        Graphics2D g = (Graphics2D) g1.create();
+
+        double dx = x2 - x1, dy = y2 - y1;
+        double angle = Math.atan2(dy, dx);
+        int len = (int) Math.sqrt(dx*dx + dy*dy);
+        AffineTransform at = AffineTransform.getTranslateInstance(x1, y1);
+        at.concatenate(AffineTransform.getRotateInstance(angle));
+        g.transform(at);
+
+        // Draw horizontal arrow starting in (0, 0)
+        g.drawLine(0, 0, len, 0);
+        g.fillPolygon(new int[] {len, len-ARR_SIZE, len-ARR_SIZE, len},
+                      new int[] {0, -ARR_SIZE, ARR_SIZE, 0}, 4);
+    }
 	public static void main(String[] args) {
-		BufferedImage bi = new BufferedImage(200, 200, BufferedImage.TYPE_INT_ARGB);
-		BufferedImage bi2 = new BufferedImage(200, 200, BufferedImage.TYPE_INT_ARGB);
+		BufferedImage bi = new BufferedImage(250, 250, BufferedImage.TYPE_INT_ARGB);
 
 		Graphics2D ig2 = bi.createGraphics(); 
-		Graphics2D ig22 = bi2.createGraphics(); 
 
-		int fontSize =20; //Taille de police défini en dur, a modifier ensuite..
+		int fontSize =50; //Taille de police défini en dur, a modifier ensuite..
 
 		Font font = new Font("TimesRoman", Font.BOLD, fontSize);
 		ig2.setFont(font);
 	    ig2.setPaint(Color.orange);
 	    
-		ig2.drawString("coucou", 2, 2);
-		ig2.translate(20, 20);
-		ig2.drawString("kikou", 50, 50);
-		ig2.translate(20, 20);
+		ig2.drawString("coucou", 100, 30);
+		ig2.drawString("kikou", 30, 100);
 		
-		java.awt.Image image = Toolkit.getDefaultToolkit().createImage(bi.getSource());
-		ig22.drawImage(image,0,0,100,100,null);
-		ig22.drawImage(image,100,0,100,100,null);
-		ig22.drawImage(image,0,100,100,100,null);
-		ig22.drawImage(image,100,100,100,100,null);
+		// draw Line2D.Double
+		//ig2.draw(new Line2D.Double(2, 2, 50, 50));
+		
+		drawArrow(ig2,100,30,30,100);
 		
 		try {
-			ImageIO.write(bi2, "PNG", new File("./output/clip.PNG"));
+			ImageIO.write(bi, "PNG", new File("./output/clip.PNG"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
+	
 
 }
