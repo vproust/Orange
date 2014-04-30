e9047a68a101dbf1cbad775e499a0c7475a770ac
Valentin <valentin-proust@wanadoo.fr>
2014-04-30 14:21:41 +0200

    intégration du parsing d'arguments

--- src/controller/main.java
+++ src/controller/main.java
@@ -5,6 +5,8 @@ package controller;
 
 import java.util.Scanner;
 
+import com.martiansoftware.jsap.*;
+
 import model.Image;
 
 /**
@@ -15,12 +17,13 @@ public class main {
 
 	/**
 	 * Appel principal du programme.
+	 * @throws JSAPException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JSAPException {
 		
 		Generator generator = new Generator();
 		
-		int mosaicWidth;
+		/** int mosaicWidth;
 		int mosaicHeight;
 		
 		Scanner sc = new Scanner(System.in);
@@ -34,9 +37,72 @@ public class main {
 		String keywords[] = new String[3];
 		keywords[0] = "action";
 		keywords[1] = "comedy";
-		keywords[2] = "drama";
+		keywords[2] = "drama";*/
 		
-		generator.generateLevels("./input/imdb_factorsColumns2.txt",image,keywords);
+		JSAP jsap = new JSAP();
+		FlaggedOption opt1 = new FlaggedOption("output")
+		.setStringParser(JSAP.STRING_PARSER)
+		.setRequired(true) 
+		.setShortFlag('o') 
+		.setLongFlag(JSAP.NO_LONGFLAG);
+
+		jsap.registerParameter(opt1);
+
+		FlaggedOption opt2 = new FlaggedOption("input")
+		.setStringParser(JSAP.STRING_PARSER)
+		.setRequired(true) 
+		.setShortFlag('i') 
+		.setLongFlag(JSAP.NO_LONGFLAG);
+
+		jsap.registerParameter(opt2);
+
+		FlaggedOption opt3 = new FlaggedOption("width")
+		.setStringParser(JSAP.INTEGER_PARSER)
+		.setDefault("1200")
+		.setRequired(false) 
+		.setShortFlag('w') 
+		.setLongFlag(JSAP.NO_LONGFLAG);
+
+		jsap.registerParameter(opt3);
+
+		FlaggedOption opt4 = new FlaggedOption("height")
+		.setStringParser(JSAP.INTEGER_PARSER)
+		.setDefault("900")
+		.setRequired(false) 
+		.setShortFlag('h') 
+		.setLongFlag(JSAP.NO_LONGFLAG);
+
+		jsap.registerParameter(opt4);
+
+		FlaggedOption opt5 = new FlaggedOption("keyWords")
+		.setStringParser(JSAP.STRING_PARSER)
+		.setRequired(false) 
+		.setShortFlag('k') 
+		.setLongFlag(JSAP.NO_LONGFLAG);
+
+		jsap.registerParameter(opt5);
+
+		JSAPResult config = jsap.parse(args);
+		System.out.println(config.getString("input"));
+		System.out.println(config.getString("output"));
+		System.out.println(config.getInt("width"));
+
+		if (!config.success()) {
+			System.err.println();
+			System.err.println("Usage: java ");
+			System.err.println("                "
+					+ jsap.getUsage());
+			System.err.println();
+			// show full help as well
+			System.err.println(jsap.getHelp());
+
+		}
+		if (config.success()) {
+		Image image = new Image(config.getInt("height"), config.getInt("width"));
+		
+		String keywords[] = config.getStringArray("keyword");
+		generator.generateLevels("./input/filmGenerateurRandomXY100.txt",image,keywords);
+		}
 	}
 
 }
--- src/laboratorie/ArgumentsParsing.java
+++ src/laboratorie/ArgumentsParsing.java
@@ -3,62 +3,62 @@ import com.martiansoftware.jsap.*;
 
 public class ArgumentsParsing {
 	public static void main(String[] args) throws JSAPException {
-JSAP jsap = new JSAP();
-        FlaggedOption opt1 = new FlaggedOption("output")
-                                .setStringParser(JSAP.STRING_PARSER)
-                                .setRequired(true) 
-                                .setShortFlag('o') 
-                                .setLongFlag(JSAP.NO_LONGFLAG);
-        
-        jsap.registerParameter(opt1);
-        
-        FlaggedOption opt2 = new FlaggedOption("input")
-        .setStringParser(JSAP.STRING_PARSER)
-        .setRequired(true) 
-        .setShortFlag('i') 
-        .setLongFlag(JSAP.NO_LONGFLAG);
+		JSAP jsap = new JSAP();
+		FlaggedOption opt1 = new FlaggedOption("output")
+		.setStringParser(JSAP.STRING_PARSER)
+		.setRequired(true) 
+		.setShortFlag('o') 
+		.setLongFlag(JSAP.NO_LONGFLAG);
 
-jsap.registerParameter(opt2);
+		jsap.registerParameter(opt1);
 
-FlaggedOption opt3 = new FlaggedOption("width")
-.setStringParser(JSAP.STRING_PARSER)
-.setRequired(false) 
-.setShortFlag('w') 
-.setLongFlag(JSAP.NO_LONGFLAG);
+		FlaggedOption opt2 = new FlaggedOption("input")
+		.setStringParser(JSAP.STRING_PARSER)
+		.setRequired(true) 
+		.setShortFlag('i') 
+		.setLongFlag(JSAP.NO_LONGFLAG);
 
-jsap.registerParameter(opt3);
+		jsap.registerParameter(opt2);
 
-FlaggedOption opt4 = new FlaggedOption("height")
-.setStringParser(JSAP.STRING_PARSER)
-.setRequired(false) 
-.setShortFlag('h') 
-.setLongFlag(JSAP.NO_LONGFLAG);
+		FlaggedOption opt3 = new FlaggedOption("width")
+		.setStringParser(JSAP.STRING_PARSER)
+		.setRequired(true) 
+		.setShortFlag('w') 
+		.setLongFlag(JSAP.NO_LONGFLAG);
 
-jsap.registerParameter(opt4);
+		jsap.registerParameter(opt3);
 
-FlaggedOption opt5 = new FlaggedOption("keyWords")
-.setStringParser(JSAP.STRING_PARSER)
-.setRequired(false) 
-.setShortFlag('k') 
-.setLongFlag(JSAP.NO_LONGFLAG);
+		FlaggedOption opt4 = new FlaggedOption("height")
+		.setStringParser(JSAP.STRING_PARSER)
+		.setRequired(false) 
+		.setShortFlag('h') 
+		.setLongFlag(JSAP.NO_LONGFLAG);
 
-jsap.registerParameter(opt5);
+		jsap.registerParameter(opt4);
 
-        JSAPResult config = jsap.parse(args);
-        System.out.println("coucou");
-        
-        if (!config.success()) {
-            System.err.println();
-            System.err.println("Usage: java ");
-            System.err.println("                "
-                                + jsap.getUsage());
-            System.err.println();
-            // show full help as well
-            System.err.println(jsap.getHelp());
-            
+		FlaggedOption opt5 = new FlaggedOption("keyWords")
+		.setStringParser(JSAP.STRING_PARSER)
+		.setRequired(false) 
+		.setShortFlag('k') 
+		.setLongFlag(JSAP.NO_LONGFLAG);
 
-        }
-		
+		jsap.registerParameter(opt5);
+
+		JSAPResult config = jsap.parse(args);
+		System.out.println("coucou");
+
+		if (!config.success()) {
+			System.err.println();
+			System.err.println("Usage: java ");
+			System.err.println("                "
+					+ jsap.getUsage());
+			System.err.println();
+			// show full help as well
+			System.err.println(jsap.getHelp());
+
+
+		}
+
 	}
 
 }
